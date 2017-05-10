3df6a0e6eabbefcb68011a7023e8612eab277e02
Mitchell Blom <mitchellblom@gmail.com>
2017-05-09 19:48:26 -0500

    writing inital objects to dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log("yay js");
+
+var app = angular.module("TodoApp", []);	// make a new prototype from angular.min.js profile. array is for plugins
+											// name in quotes must be the same as the html. THE SAME!!!
+
+app.controller("NavCtrl", ($scope) => {			//quotes are name of controller, usually has ctrl in it in PaschalCase
+	$scope.cat = "Meow";						// angular variables to use go in parens
+	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});	
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.dog = "Woof!";
+})
