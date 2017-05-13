7b368bdcc608a3560a67f24bd901347c7932822c
webbdm <gwebb762@gmail.com>
2017-05-13 10:58:30 -0500

    Added AppConstants

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log("yay js");
+app.controller("NavCtrl", ($scope) => { //quotes are name of controller, usually has ctrl in it in PaschalCase
+    $scope.cat = "Meow"; // angular variables to use go in parens
+    $scope.navItems = [{ name: "Logout" }, { name: "All Items" }, { name: "New Item" }];
+});
+
+app.controller("ItemCtrl", ($scope) => {
+    $scope.dog = "Woof!";
+    $scope.showListView = true;
+    $scope.items = [];
+
+    $scope.newItem = () => {
+        $scope.showListView = false;
+    };
+
+    $scope.allItems = () => {
+        $scope.showListView = true;
+    };
+
+});
